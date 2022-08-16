8b9abcf7e3dbf79f250675bc876e91e8b647ef12
Denis <denismunene999@gmail.com>
2022-08-16 14:51:57 +0300

    commit tree a58f61b9964cbbde81999751cead27ae8a1b43bb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+
+#greetProgrammer
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+
+#greet
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+#greetWithDefault
+def greet_with_default(name="programmer")
+  puts "Hello, #{name}!"
+end
+
+#add
+def add num1,num2
+  num1 + num2
+end
+
+#halve
+def halve number
+  if number.class != Integer
+    return nil
+  end
+
+  number / 2
+end
